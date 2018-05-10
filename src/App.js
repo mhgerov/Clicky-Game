69e5d9a552e421a7995a6b350f1919c7bfebe786
Michael Gerov <mhgerov@gmail.com>
2018-05-10 12:42:19 -0400

    built component folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import title from './components/index.js';
+
+class title extends React.Component {
+	render() {
+		return (
+			<h1>Hello, Friend!</h1>
+		);
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div>
+				<title />
+				<p>This is some text</p>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class title extends React.Component {
+	render() {
+		return (
+			<h1>Hello, Friend!</h1>
+		);
+	}
+}
+
+export default title;
